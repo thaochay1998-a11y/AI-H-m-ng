d87b8989315038a1aa18f6de61a489a5a77a7f00
thaochay1998-a11y <thaochay1998@gmail.com>
2026-08-29 21:04:27 +0700

    Create page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', color: 'white', textAlign: 'center' }}>
+      <h1>AI H'Mông</h1>
+      <p>Ứng dụng đã hoạt động thành công!</p>
+    </div>
+  )
+}
